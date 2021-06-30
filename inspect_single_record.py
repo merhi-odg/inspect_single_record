88c7e6fda118b91d19f92c0e5ba9f043b7751b60
Sami Merhi <51339977+merhi-odg@users.noreply.github.com>
2021-06-30 15:22:36 -0500

    Update inspect_single_record.py

--- inspect_single_record.py
+++ inspect_single_record.py
@@ -7,23 +7,26 @@ def begin():
 
 # modelop.score
 def action(data):
+    
+    print("\nData Received: ", data, flush=True)
+    print(flush=True)
   
     # input data should be a dictionary, such as {"a":1, "b": "foo"}
   
     data = pandas.DataFrame([data])
 
     print("Pandas Version: ", pandas.__version__, flush=True)
-    print()
+    print(flush=True)
     print("Numpy  Version: ", numpy.__version__, flush=True)
-    print()
+    print(flush=True)
 
     print("\nData Shape: ", data.shape, flush=True)
-    print()
+    print(flush=True)
     print("\nData Types: ", data.dtypes.to_dict(), flush=True)
-    print()
+    print(flush=True)
     print("\nSample Rows: ", data.head(), flush=True)
-    print()
+    print(flush=True)
     print("\nNulls per Column: ", data.isna().sum().to_dict(), flush=True)
-    print()
+    print(flush=True)
 
     yield data.to_dict(orient='records')
